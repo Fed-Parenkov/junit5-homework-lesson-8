065b95a832b438ac22a37786cb36ec59c68b0017
ParenkovLocal <parenkov.fedor@gmail.com>
2021-09-30 15:09:21 +0400

    Complete code without page objects

--- src/test/java/parenkov/tests/ParametrizedTests.java
+++ src/test/java/parenkov/tests/ParametrizedTests.java
@@ -15,21 +15,23 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class ParametrizedTests {
 
+    @ValueSource(strings = {"Development", "Design", "PopSci"})
+
     @ParameterizedTest(name = "Switching to the header menu item: {0}")
-    @ValueSource(strings = {"Разработка", "Дизайн", "Научпоп"})
     void habrHeaderMenuVSTest(String menuItem) {
         Configuration.startMaximized = true;
-        open("https://habr.com/ru/all/");
+        open("https://habr.com/en/all/");
         $(".tm-main-menu__section-content").$(byText(menuItem)).click();
         $("h1").shouldHave(text(menuItem));
     }
 
-    @ParameterizedTest(name = "Fill form with name: {2}")
     @CsvSource(value = {
             "01; Tesla; Elon Musk; elon; 123456789",
             "02; Apple; Tim Cook; cook; 123456789",
             "03; Microsoft; Bill Gates; gates; 123456789"
     }, delimiter = ';')
+
+    @ParameterizedTest(name = "Fill form with name: {2}")
     void fillFormCSVTest(int testId,
                          String company,
                          String name,
@@ -52,18 +54,18 @@ public class ParametrizedTests {
         $("h2").shouldHave(text("Welcome to your Simple In/Out free trial"));
     }
 
-    @ParameterizedTest(name = "Switching to the header menu item: {0}")
     @EnumSource(value = HeaderMenuItem.class,
             mode = EnumSource.Mode.EXCLUDE,
             names = {"DESIGN", "MANAGEMENT"}
     )
+
+    @ParameterizedTest(name = "Switching to the header menu item: {0}")
     void habrHeaderMenuESTest(HeaderMenuItem menuItem) {
         Configuration.startMaximized = true;
         open("https://habr.com/ru/all/");
         $(".tm-main-menu__section-content").$(byText(menuItem.getDesc())).click();
         $("h1").shouldHave(text(menuItem.getDesc()));
     }
-
 
     static Stream<Arguments> testData() {
         return Stream.of(
@@ -79,8 +81,9 @@ public class ParametrizedTests {
         );
     }
 
-    @ParameterizedTest(name = "Fill form with name: {2}")
     @MethodSource("testData")
+
+    @ParameterizedTest(name = "Fill form with name: {2}")
     void fillFormMSTest(int testId,
                         String company,
                         String name,
